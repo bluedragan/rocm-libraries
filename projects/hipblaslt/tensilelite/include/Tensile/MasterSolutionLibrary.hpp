7e299053d42513a8a43ee348430159fdfe9651d1
Ruban Kumar <ruban.kumar@amd.com>
2025-11-25 22:32:17 -0600

    Complex Datatype Support in hipBLASLt

--- projects/hipblaslt/tensilelite/include/Tensile/MasterSolutionLibrary.hpp
+++ projects/hipblaslt/tensilelite/include/Tensile/MasterSolutionLibrary.hpp
@@ -199,7 +199,10 @@ namespace TensileLite
             {
                 return std::shared_ptr<MySolution>();
             }
-            auto solution = solutions.at(index);
+            auto                   solution = solutions.at(index);
+            
+            TensileLite::TensorOps nop;
+
             if(solution->requiredHostWorkspaceSizePerProblem == static_cast<size_t>(-1))
             {
                 auto problem
@@ -209,8 +212,8 @@ namespace TensileLite
                                                       solution->problemType.bType,
                                                       solution->problemType.cType,
                                                       solution->problemType.dType,
-                                                      solution->problemType.computeType,
-                                                      solution->problemType.computeType,
+                                                      solution->problemType.aType,
+                                                      solution->problemType.aType,
                                                       solution->problemType.computeInputType,
                                                       solution->problemType.computeType,
                                                       1.0,
@@ -220,7 +223,8 @@ namespace TensileLite
                                                       solution->problemType.biasDataTypeWhiteList,
                                                       solution->problemType.biasSrcWhiteList,
                                                       solution->problemType.groupedGemm,
-                                                      std::numeric_limits<size_t>::max());
+                                                      std::numeric_limits<size_t>::max(),
+                                                      nop,nop,nop,nop);
                 solution->requiredHostWorkspaceSizePerProblem
                     = solution->requiredHostSizeGroupedGemmSingle(problem, hardware);
             }
